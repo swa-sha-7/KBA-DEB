04a55655f71b8fd59813ee8d54c68c8fdd4c26e6
swa-sha-7 <swatishania@gmail.com>
2024-11-26 10:46:01 +0530

    Update Home.jsx

--- VehicleMS_React/UI/src/pages/Home.jsx
+++ VehicleMS_React/UI/src/pages/Home.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import Hero from '../components/Hero'
-import TopCourses from '../components/TopCourses'
-import CourseGrid from '../components/CourseGrid'
 import ViewAllVehiclesButton from '../components/ViewAllVehiclesButton'
 
 const Home = () => {
   
   return (
    <>
-      {/* <Hero />
-      <TopCourses />
-      <CourseGrid isHome={true} /> */}
       <ViewAllVehiclesButton />
    </>
   )
